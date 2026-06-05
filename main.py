d3c661e95e7930b7fabd3b52bd6a20a2865c93a6
Kyryora <rizkywicaksono24@gmail.com>
2026-06-05 13:19:44 +0700

    Validasi batasan input di main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import csv
+from tracemalloc import start
 
 border = "─" * 45
 
@@ -22,22 +23,39 @@ def main_menu():
         print(f"{i:2}: {tempat}")
     
     try:
-        start = input("\n>> ID Titik Awal: ")
-        end = input(">> ID Tujuan: ")
+        start = int(input("\n>> ID Titik Awal: "))
+        end = int(input(">> ID Tujuan: "))
+        
+        if start < 0 or start >= len(nama_tempat):
+            print("ID Titik tidak valid!")
+            return
+
+        if end < 0 or end >= len(nama_tempat):
+            print("ID tidak valid!")
+            return
+        
+        if start == end:
+            print("Titik Awal dan Tujuan tidak boleh sama!")
+            return
         
         # Panggil Engine C++
-        process = subprocess.run(['./engine.exe', start, end], capture_output=True, text=True)
-        jarak = float(process.stdout.strip())
+        process = subprocess.run(['./engine.exe', str(start), str(end)], capture_output=True, text=True)
+        hasil = process.stdout.strip().split('\n')
+
+        jarak = float(hasil[0])
+        rute = hasil[1]
 
         if process.stderr:
             print(" ")
             print(process.stderr)
         
         print(f"\n✔ Rute Ditemukan!")
+        print(f"Rute: {rute}")
         print(f"Total Jarak: {jarak:.2f} km")
         print(f"Estimasi: {int(jarak * 10)} - {int(jarak * 15)} Menit")
-    except:
-        print("Error: Pastikan engine.exe sudah dikompilasi!")
+    except Exception as e:
+        print("Terjadi error:")
+        print(e)
 
 if __name__ == "__main__":
     while True:
